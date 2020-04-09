045ea6a7e7520df3d1fd27a7f31cea83b0a8b995
Marcus Vinicius Pimenta <mvinicius.pimenta@gmail.com>
2020-04-09 19:25:38 +0100

    Create the about activity layout

--- features/about/src/main/java/com/pimenta/covid19/about/presentation/ui/activity/AboutActivity.kt
+++ features/about/src/main/java/com/pimenta/covid19/about/presentation/ui/activity/AboutActivity.kt
@@ -14,10 +14,17 @@
 
 package com.pimenta.covid19.about.presentation.ui.activity
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pimenta.covid19.about.R
 
 /**
  * Created by marcus on 09-04-2020.
  */
 class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
 }
