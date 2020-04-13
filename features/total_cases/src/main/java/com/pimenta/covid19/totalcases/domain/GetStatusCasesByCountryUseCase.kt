3195e73c8b1fe9c34cdf011c595e9e468af2ea9c
Marcus Vinicius Pimenta <mvinicius.pimenta@gmail.com>
2020-04-13 10:15:23 +0100

    Create the domain layer about total cases feature

---
+++ features/total_cases/src/main/java/com/pimenta/covid19/totalcases/domain/GetStatusCasesByCountryUseCase.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2020 Marcus Pimenta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.pimenta.covid19.totalcases.domain
+
+import com.pimenta.covid19.model.domain.CountryCaseDomainModel
+import com.pimenta.covid19.totalcases.data.repository.TotalCasesRepositoryInterface
+import io.reactivex.Single
+import io.reactivex.functions.Function3
+import javax.inject.Inject
+
+/**
+ * Created by marcus on 13-04-2020.
+ */
+private const val CONFIRMED = "confirmed"
+private const val RECOVERED = "recovered"
+private const val DEATHS = "deaths"
+
+class GetStatusCasesByCountry @Inject constructor(
+    private val totalCasesRepositoryInterface: TotalCasesRepositoryInterface
+) : GetStatusCasesByCountryUseCaseInterface {
+
+    override fun invoke(
+        slug: String
+    ): Single<Triple<List<CountryCaseDomainModel>?, List<CountryCaseDomainModel>?, List<CountryCaseDomainModel>?>> =
+        Single.zip(
+            totalCasesRepositoryInterface.getTotalCasesByType(slug, CONFIRMED),
+            totalCasesRepositoryInterface.getTotalCasesByType(slug, RECOVERED),
+            totalCasesRepositoryInterface.getTotalCasesByType(slug, DEATHS),
+            Function3 { confirmed, recovered, deaths ->
+                Triple(confirmed, recovered, deaths)
+            }
+        )
+
+}
